d54dae3c43e7194c6ebd6290b3d38b1e56082389
jkyony <jkyony@gmail.com>
2016-09-02 11:16:07 +0100

    the fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [:user, :admin]
+  enum role: [:user,:curator, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
